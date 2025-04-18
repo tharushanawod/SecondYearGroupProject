77943d221873d96d1a3cc93cc1348f27588a10ec
Tharusha Nawod <tharushanawod777@gmail.com>
2025-04-18 17:44:05 +0530

    moderator profile issues fixed

--- app/models/Moderator.php
+++ app/models/Moderator.php
@@ -21,6 +21,38 @@ class Moderator{
        
     }
 
+    public function UpdateProfile($data) {
+        if (!empty($data['password'])) {
+            // Include password in the update query
+            $this->db->query('UPDATE users SET name = :name, email = :email, phone = :phone,  password = :password WHERE user_id = :id');
+            $this->db->bind(':password', $data['password']);
+        } else {
+            // Exclude password from the update query
+            $this->db->query('UPDATE users SET name = :name, email = :email, phone = :phone WHERE user_id = :id');
+        }
+    
+     
+        $this->db->bind(':id', $data['user_id']);
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':phone', $data['phone']);
+       
+    
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUserById ($id){
+        $this->db->query('SELECT * FROM users WHERE user_id = :id');
+        $this->db->bind(':id',$id);
+        $row = $this->db->single();
+        return $row;
+    }
+
 
 }
 ?>
